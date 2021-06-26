6db9fb1b3705804189da895300cfd6ccc32fce6d
eEqualsMxC <justinmustaine@gmail.com>
2021-06-26 16:21:00 -0400

    cleaning again

--- scribe/tests/tests.py
+++
@@ -1,9 +0,0 @@
-import scribe
-
-# Test Scribe_Reader.read_from_csv()
-file_path = r"C:\Users\justi\Desktop\scribe\scribe\test_data\large_sample.csv"
-scribe = scribe.Scribe_Reader()
-# scribe.read_from_csv(file_path)
-# print(scribe.columns)
-# print(scribe.dtypes)
-# print(scribe.shape)
