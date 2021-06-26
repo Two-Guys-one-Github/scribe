1ad1cea3fff774c473248e37f494693eb5fe0ab7
eEqualsMxC <justinmustaine@gmail.com>
2021-06-26 17:33:56 -0400

    ok

---
+++ scribe/test.py
@@ -0,0 +1,14 @@
+from . import scribe_readers
+
+
+# file_path_001 = r"C:\Users\justi\Desktop\scribe\scribe\test_materials\test_data\large_sample.csv"
+# file_path_002 = r"C:\Users\justi\Desktop\scribe\scribe\test_materials\test_data\names.csv"
+# file_path_002 = r"C:\Users\justi\Desktop\scribe\scribe\test_materials\test_data\new_names.csv"
+                  
+# # Test Scribe_Reader.read_from_csv()
+# def test_scribe_file_Reader_001(file_path):
+#     scribe = scribe_readers.Scribe_File_Reader()
+#     scribe.read_from_csv(file_path)
+#     print(scribe.columns)
+#     print(scribe.dtypes)
+#     print(scribe.shape)
